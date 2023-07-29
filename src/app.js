7fdd5497643a4cc2e78308ad6418b426949c207e
Darryl <dwsmorris@hotmail.com>
2023-07-29 11:35:11 +0100

    adjust svg coordinate frame on resize

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from "styled-components";
 
 const StyledSvg = styled.svg`
@@ -6,14 +6,40 @@ const StyledSvg = styled.svg`
 		fill: red;
 	}
 `;
+const maxY = 200;
 
 export default () => {
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
+	const aspect = windowSize.width / windowSize.height;
+	const maxX = aspect * maxY;
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		};
+
+		// Attach the event listener
+		window.addEventListener('resize', handleResize);
+
+		// Clean up the event listener when the component is unmounted
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []); // Empty dependency array to ensure this effect only runs once
+
+
 	return <StyledSvg
 		className="svg"
 		xmlns="http://www.w3.org/2000/svg"
 		width="100%"
 		height="100vh"
-		viewBox="-100 -100 200 200">
-		<circle cx="0" cy="0" r="1"/>
+		viewBox={`${-maxX} ${-maxY} ${2 * maxX} ${2 * maxY}`}>
+		<circle cx="0" cy="0" r="5"/>
 	</StyledSvg>;
 };
