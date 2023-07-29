aedc7dcae1df43906c81c598330d8b2551b3aef9
Darryl <dwsmorris@hotmail.com>
2023-07-29 01:45:36 +0100

    add styled components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from "styled-components";
 
-export default () => <svg
-	className="svg"
-	xmlns="http://www.w3.org/2000/svg"
-	width="100%"
-	height="100vh"
-	viewBox="0 0 100 100">
-	<circle cx="50" cy="50" r="1" fill="red"/>
-</svg>;
+const StyledSvg = styled.svg`
+	> circle {
+		fill: red;
+	}
+`;
+
+export default () => {
+	return <StyledSvg
+		className="svg"
+		xmlns="http://www.w3.org/2000/svg"
+		width="100%"
+		height="100vh"
+		viewBox="0 0 100 100">
+		<circle cx="50" cy="50" r="1"/>
+	</StyledSvg>;
+};
