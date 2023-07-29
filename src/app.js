2c59318b7ad72e4f913e02e4a720c9e04444ff9d
Darryl <dwsmorris@hotmail.com>
2023-07-29 01:24:38 +0100

    update circle

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const App = () => {
-	return (
-		<div>
-			<h1>Hello, React! 2</h1>
-		</div>
-	);
-};
-
-export default App;
+export default () => <svg
+	className="svg"
+	xmlns="http://www.w3.org/2000/svg"
+	width="100%"
+	height="100vh"
+	viewBox="0 0 100 100">
+	<circle cx="50" cy="50" r="1" fill="red"/>
+</svg>;
