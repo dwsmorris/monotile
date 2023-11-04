c9ba1fc4f38bfaebab23e322b4752eed5f1a9e1b
Darryl Morris <dwsmorris@hotmail.com>
2023-11-04 15:55:40 +0000

    use source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: 'development', // Set the build mode to 'development'
+	devtool: "source-map",
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
